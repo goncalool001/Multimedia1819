745973364f3a2a97c4e570169778e9c0cae370b6
Fábio Cordeiro <uc2016242575@student.uc.pt>
2019-03-09 11:50:27 +0000

    todos os button funcionais exceto o do slideshow

---
+++ TP3/code/page2.js
@@ -0,0 +1,159 @@
+"use strict";
+
+var fotoAtual = 1;
+const totalFotos = 16;
+const opacDisabled = 0.3;  //transparência para botões desactivados
+const imgFolder = "../resources/image/";
+const txtFolder = "../resources/text/";
+var audioVolume = 1;
+
+
+(function()
+{
+	window.addEventListener("load", main);
+}());
+
+
+function main()
+{
+	var image = document.getElementById("photo");
+	var text = document.getElementById("text");
+	var firstPageButton = document.getElementById("firstBtn");
+	var backButton = document.getElementById("backBtn");
+	var nextButton = document.getElementById("nextBtn");
+	var lastPageButton = document.getElementById("lastBtn");
+	var slideshowButton = document.getElementById("slideShowBtn");
+	var soundButton = document.getElementById("soundBtn");
+	image.src = imgFolder + "0" + fotoAtual + ".jpg";
+	text.src = txtFolder + "0" + fotoAtual + ".txt";
+	firstPageButton.style.opacity = opacDisabled;
+	firstPageButton.disabled = true;
+	backButton.style.opacity = opacDisabled;
+	backButton.disabled = true;
+	firstPageButton.addEventListener("click",showFirstPage);
+	lastPageButton.addEventListener("click",showLastPage);
+	nextButton.addEventListener("click",showNextPage);
+	backButton.addEventListener("click",showPageBefore);
+	soundButton.addEventListener("click",muteUnmuteMusic);
+}
+
+function showFirstPage(ev){
+	var image = document.getElementById("photo");
+	var text = document.getElementById("text");
+	var firstPageButton = document.getElementById("firstBtn");
+	var lastPageButton = document.getElementById("lastBtn");
+	var backButton = document.getElementById("backBtn");
+	var nextButton = document.getElementById("nextBtn");
+	fotoAtual = 1;
+	image.src = imgFolder + "0" + fotoAtual + ".jpg";
+	text.src = txtFolder + "0" + fotoAtual + ".txt";
+	firstPageButton.style.opacity = opacDisabled;
+	firstPageButton.disabled = true;
+	nextButton.style.opacity = 1;
+	nextButton.disabled = false;
+	lastPageButton.style.opacity = 1;
+	lastPageButton.disabled = false;
+	backButton.style.opacity = opacDisabled;
+	backButton.disabled = true;
+
+}
+
+function showLastPage(ev){
+	var image = document.getElementById("photo");
+	var text = document.getElementById("text");
+	var firstPageButton = document.getElementById("firstBtn");
+	var lastPageButton = document.getElementById("lastBtn");
+	var backButton = document.getElementById("backBtn");
+	var nextButton = document.getElementById("nextBtn");
+	fotoAtual = totalFotos;
+	image.src = imgFolder + fotoAtual + ".jpg";
+	text.src = txtFolder + fotoAtual + ".txt";
+	firstPageButton.style.opacity = 1;
+	firstPageButton.disabled = false;
+	backButton.style.opacity = 1;
+	backButton.disabled = false;
+	lastPageButton.style.opacity = opacDisabled;
+	lastPageButton.disabled = true;
+	nextButton.style.opacity = opacDisabled;
+	nextButton.disabled = true;
+}
+
+function showNextPage(ev){
+	var image = document.getElementById("photo");
+	var text = document.getElementById("text");
+	var firstPageButton = document.getElementById("firstBtn");
+	var lastPageButton = document.getElementById("lastBtn");
+	var backButton = document.getElementById("backBtn");
+	var nextButton = document.getElementById("nextBtn");
+	if(fotoAtual<totalFotos && fotoAtual != totalFotos-1){
+		fotoAtual++;
+		backButton.style.opacity = 1;
+		backButton.disabled = false;
+		firstPageButton.style.opacity = 1;
+		firstPageButton.disabled = false;
+		if(fotoAtual>9){
+			image.src = imgFolder + fotoAtual + ".jpg";
+			text.src = txtFolder + fotoAtual + ".txt";
+		}
+		else{
+			image.src = imgFolder + "0" + fotoAtual + ".jpg";
+			text.src = txtFolder + "0" + fotoAtual + ".txt";
+		}
+	}
+	if(fotoAtual==totalFotos-1){
+		image.src = imgFolder + totalFotos + ".jpg";
+		text.src = txtFolder + totalFotos + ".txt";
+		lastPageButton.style.opacity = opacDisabled;
+		lastPageButton.disabled = true;
+		nextButton.style.opacity = opacDisabled;
+		nextButton.disabled = true;
+	}
+}
+
+function showPageBefore(ev){
+	var image = document.getElementById("photo");
+	var text = document.getElementById("text");
+	var firstPageButton = document.getElementById("firstBtn");
+	var lastPageButton = document.getElementById("lastBtn");
+	var backButton = document.getElementById("backBtn");
+	var nextButton = document.getElementById("nextBtn");
+	if(fotoAtual>1 && fotoAtual != 2){
+		nextButton.style.opacity = 1;
+		nextButton.disabled = false;
+		fotoAtual--;
+		lastPageButton.style.opacity = 1;
+		lastPageButton.disabled = false;
+		if(fotoAtual>9){
+			image.src = imgFolder + fotoAtual + ".jpg";
+			text.src = txtFolder + fotoAtual + ".txt";
+		}
+		else{
+			image.src = imgFolder + "0" + fotoAtual + ".jpg";
+			text.src = txtFolder + "0" + fotoAtual + ".txt";
+		}
+	}
+	if(fotoAtual==2){
+		image.src = imgFolder + "01" + ".jpg";
+		text.src = txtFolder + "01" + ".txt";
+		firstPageButton.style.opacity = opacDisabled;
+		firstPageButton.disabled = true;
+		backButton.style.opacity = opacDisabled;
+		backButton.disabled = true;
+	}
+}
+
+function muteUnmuteMusic(ev){
+	var soundButton = document.getElementById("soundBtn");
+	var buttonImage = soundButton.getElementsByTagName("img")[0];
+	var music = document.getElementsByTagName("audio")[0];
+	if(audioVolume==1){
+		audioVolume = 0;
+		music.muted = true;
+		buttonImage.src = "../resources/extra/soundOffBtn.png";
+	}
+	else{
+		audioVolume = 1;
+		music.muted = false;
+		buttonImage.src = "../resources/extra/soundOnBtn.png";
+	}
+}
