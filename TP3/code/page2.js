fe8f5613c28ec72c68202d89c357f4ac3bc21e94
Gonçalo Sousa <goncalooliveirasousa@gmail.com>
2019-03-12 01:28:14 +0000

    slideshow

--- TP3/code/page2.js
+++ TP3/code/page2.js
@@ -35,7 +35,56 @@ function main()
 	nextButton.addEventListener("click",showNextPage);
 	backButton.addEventListener("click",showPageBefore);
 	soundButton.addEventListener("click",muteUnmuteMusic);
+	slideshowButton.addEventListener("click",slideShow);
 }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function slideShow(ev) {
+	var image = document.getElementById("photo");
+	var text = document.getElementById("text");
+	var firstPageButton = document.getElementById("firstBtn");
+	var lastPageButton = document.getElementById("lastBtn");
+	var backButton = document.getElementById("backBtn");
+	var nextButton = document.getElementById("nextBtn");
+	var slideshowButton = document.getElementById("slideShowBtn");
+
+	nextButton.disabled = true;
+	lastPageButton.disabled = true;
+	backButton.disabled = true;
+	firstPageButton.disabled = true;
+	slideshowButton.disabled = true;
+
+	lastPageButton.style.opacity = opacDisabled;
+	nextButton.style.opacity = opacDisabled;
+	backButton.style.opacity = opacDisabled;
+	firstPageButton.style.opacity = opacDisabled;
+	slideshowButton.style.opacity = opacDisabled;
+	for(fotoAtual =1; fotoAtual<totalFotos ;fotoAtual++){
+		if(fotoAtual != totalFotos-1){
+			if(fotoAtual>9){
+				image.src = imgFolder + fotoAtual + ".jpg";
+				text.src = txtFolder + fotoAtual + ".txt";
+			}
+			else{
+				image.src = imgFolder + "0" + fotoAtual + ".jpg";
+				text.src = txtFolder + "0" + fotoAtual + ".txt";
+			}
+		}
+		if(fotoAtual==totalFotos-1){
+			image.src = imgFolder + totalFotos + ".jpg";
+			text.src = txtFolder + totalFotos + ".txt";
+		}
+		 await sleep(2000);
+		}
+		firstPageButton.style.opacity = 1;
+		firstPageButton.disabled = false;
+		backButton.style.opacity = 1;
+		backButton.disabled = false;
+		slideshowButton.style.opacity = 1;
+		slideshowButton.disabled = false;
+	}
 
 function showFirstPage(ev){
 	var image = document.getElementById("photo");
